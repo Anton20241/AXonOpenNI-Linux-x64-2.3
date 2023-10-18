24f7e454dac57452c0f0f6d0b69bfa0afaec3be0
Anton20241 <ilyasov.toni@mail.ru>
2023-10-18 18:59:36 +0300

    stable

---
+++ AXon_user/build/CMakeFiles/AXon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AXon"
+  "AXon.pdb"
+  "CMakeFiles/AXon.dir/Viewer.cpp.o"
+  "CMakeFiles/AXon.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AXon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
